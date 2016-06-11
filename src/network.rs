c7022e486e67c97bde3c0c58b0b44e67ba92de0b
Til Blechschmidt <til@blechschmidt.de>
2016-06-11 15:13:47 +0200

    Bugfix for test

--- src/network.rs
+++ src/network.rs
@@ -207,5 +207,5 @@ fn recursive_evaluation() {
     let mut net = Network::new_empty(1, 1);
     let res = net.nodes[1].evaluate();
     net.nodes[1].reset();
-    assert!(res != net.evaluate(vec![0.5]).unwrap()[0]);
+    assert!(res != net.evaluate(&vec![0.5]).unwrap()[0]);
 }
